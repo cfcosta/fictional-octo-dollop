e6cd649982dd3d18c8cbf100aaac392f7a223e8f
Cainã Costa <me@cfcosta.com>
2025-10-21 08:48:22 -0300

    feat: initial data structure, dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+pub type Id = u16;
+
+pub enum InputType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+pub struct Input {
+    pub kind: InputType,
+    pub client: Id,
+    pub transaction: Id,
+    pub amount: u64,
+}
+
+pub struct State {
+    pub id: Vec<Id>,
+    pub available: Vec<u64>,
+    pub held: Vec<u64>,
+    pub locked: Vec<bool>,
+}
+
+fn apply(state: &mut State, input: Input) {
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
 }
